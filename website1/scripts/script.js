fd88fc880e36743e6f68e05ef2732dbfc9aa0c95
Adriel <agimenescomercial@gmail.com>
2022-04-22 15:43:57 -0300

    v0.11

--- website1/scripts/script.js
+++
@@ -1,9 +0,0 @@
-/*W*/
-
-const toggleMenu = document.querySelector('#mobile-menu')
-const menuLinks = document.querySelector('.navbar-menu')
-
-toggleMenu.addEventListener('click', function() {
-    toggleMenu.classList.toggle('is-active');
-    menuLinks.classList.toggle('active');
-})
