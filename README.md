11b084d845843427e5bfa31659d082a7cf1be6f4
Adriel <agimenescomercial@gmail.com>
2022-05-17 20:20:30 -0300

    v0.03

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
-Hello and welcome to my Portfolio
+#Adriel's Portfolio
 
-[Insert text here]
+Hi, Hello and welcome to my Portfolio!
+
+Access [LINK] to visit the main page of my portfolio.
+
+There, you'll find everything I've done organized into categories.
+
+Feel free to contact me through the links provided.
+
--- Website 1/scripts/script.js
+++ Website 1/scripts/script.js
@@ -1,5 +1,6 @@
 /*W*/
 
+/******* NAVBAR DROPDOWN MENU ACTIVATOR *******/
 const toggleMenu = document.querySelector('#mobile-menu')
 const menuLinks = document.querySelector('.navbar-menu')
 
@@ -7,3 +8,23 @@ toggleMenu.addEventListener('click', function() {
     toggleMenu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
 })
+/******* END OFNAVBAR DROPDOWN MENU ACTIVATOR *******/
+
+
+/******* SLIDER MANAGEMENT *******/
+let count = 1;
+document.getElementById('radio1').checked = true;
+
+setInterval( function() {
+    nextImage();
+}, 7000);
+
+function nextImage() {
+    count++;
+    if(count > 3) {
+        count = 1;
+    }
+
+    document.getElementById('radio' + count).checked = true;
+}
+/******* END OF SLIDER MANAGEMENT *******/
